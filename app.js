36f5eb92f7f11f78a8bb582f590eee6606ad402a
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2023-03-28 11:09:25 -0700

    dan

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ async function list_files() {
   // while (true) {
 
   let response = await retrier
-    .resolve(fn)
+    .resolve(attempt => fn)
     .then(
       result => {console.log(result);return result},
       error => {console.error(error) ;return error}
